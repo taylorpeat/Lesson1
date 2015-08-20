4a1d0517022f79653a737f4268ae9df58f8eac55
Taylor Peat <taylorpeat@hotmail.com>
2015-08-20 01:55:41 -0400

    Recommended revisions

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,4 @@
+require 'pry'
 CARD_FORMAT = [" _________ ", "|%{num}%{n10}%{c8}   %{c8}  |", "|%{suit} %{c6} %{c10} %{c6}  |",
                "|  %{c4} %{c7} %{c4}  |", "|  %{c6} %{c9} %{c6}  |", "|  %{c4} %{c2} %{c4}  |",
                "|  %{c6} %{c10} %{c6} %{suit}|", "|  %{c8}   %{c8}%{n10}%{num}|", " --------- "]
@@ -7,7 +8,7 @@ VALUES = [["2", 2], ["3", 3], ["4", 4], ["5", 5], ["6", 6], ["7", 7], ["8", 8], 
 
 def initialize_deck
   deck = SUITS.product(VALUES).each { |card| card.flatten! }
-  deck = shuffle_deck(deck)
+  deck.shuffle!
 end
 
 def initialize_variables
@@ -18,77 +19,6 @@ def initialize_variables
   [deck, player_cards, player_cards2, dealer_cards, player_total, player_total2, dealer_total, bet, bet2, stand, stand2, continue]
 end
 
-def shuffle_deck(deck)
-  shuffled_deck = []
-  loop do
-    next_card = rand(deck.length)
-    shuffled_deck << deck.delete_at(next_card)
-    break if deck.length < 1
-  end
-  shuffled_deck
-end
-
-def update_card_values(card)
-  c2 = c4 = c6 = c7 = c8 = c9 = c10 = n10 = " "
-  num = card[1]
-  case card[0]
-  when :spades then suit = "\u2660"
-  when :clubs then suit = "\u2663"
-  when :hearts then suit = "\u2665"
-  when :diamonds then suit = "\u2666"
-  end
-  case card[1]
-  when "2" then c2 = c7 = suit
-  when "3" then c9 = c10 = suit
-  when "4" then c4 = suit
-  when "5" then c4 = c9 = suit
-  when "6" then c6 = suit
-  when "7" then c6 = c7 = suit
-  when "8" then c4 = c8 = suit
-  when "9" then c4 = c8 = c9 = suit
-  when "10"
-    c4 = c8 = c10 = suit
-    n10 = ""
-  when "ace"
-    num = "A"
-    c9 = suit
-  when "king"
-    num = "K"
-    c9 = suit
-  when "queen"
-    num = "Q"
-    c9 = suit
-  when "jack"
-    num = "J"
-    c9 = suit
-  end
-  [suit, num, c2, c4, c6, c7, c8, c9, c10, n10]
-end
-
-def compile_lines(lines, cv)
-  lines[0] << CARD_FORMAT[0]
-  lines[1] << format(CARD_FORMAT[1], num: cv[1], c8: cv[6], n10: cv[9])
-  lines[2] << format(CARD_FORMAT[2], suit: cv[0], c6: cv[4], c10: cv[8])
-  lines[3] << format(CARD_FORMAT[3], c4: cv[3], c7: cv[5])
-  lines[4] << format(CARD_FORMAT[4], c6: cv[4], c9: cv[7])
-  lines[5] << format(CARD_FORMAT[5], c2: cv[2], c4: cv[3])
-  lines[6] << format(CARD_FORMAT[6], suit: cv[0], c6: cv[4], c10: cv[8])
-  lines[7] << format(CARD_FORMAT[7], num: cv[1], c8: cv[6], n10: cv[9])
-  lines[8] << CARD_FORMAT[8]
-end
-
-def compile_lines_hidden(lines, cv)
-  lines[0] << CARD_FORMAT[0].slice(0..2)
-  lines[1] << format(CARD_FORMAT[1], num: cv[1], c8: cv[6], n10: cv[9]).slice(0..2)
-  lines[2] << format(CARD_FORMAT[2], suit: cv[0], c6: cv[4], c10: cv[8]).slice(0..2)
-  lines[3] << format(CARD_FORMAT[3], c4: cv[3], c7: cv[5]).slice(0..2)
-  lines[4] << format(CARD_FORMAT[4], c6: cv[4], c9: cv[7]).slice(0..2)
-  lines[5] << format(CARD_FORMAT[5], c2: cv[2], c4: cv[3]).slice(0..2)
-  lines[6] << format(CARD_FORMAT[6], suit: cv[0], c6: cv[4], c10: cv[8]).slice(0..2)
-  lines[7] << format(CARD_FORMAT[7], num: cv[1], c8: cv[6], n10: cv[9]).slice(0..2)
-  lines[8] << CARD_FORMAT[8].slice(0..2)
-end
-
 def update_display(name, balance, player_cards, player_cards2, dealer_cards, player_total, player_total2,
                    dealer_total, stand, stand2, bet, bet2)
   if stand && (stand2 || player_total2 == 0) && !player_doesnt_care?(player_total, player_total2, dealer_total)
@@ -97,7 +27,7 @@ def update_display(name, balance, player_cards, player_cards2, dealer_cards, pla
     dealer_total = dealer_cards[0][2] unless dealer_cards.empty?
     dealer_cards =  dealer_cards.take(1) unless dealer_cards.empty?
   end
-  system 'clear'
+  system 'clear' or system 'cls'
   puts "Balance: $#{balance}" + " " * 10 + "--TEALEAF BLACKJACK--"
   puts "Wager:   $#{bet + bet2}"
   puts "\nPlayer Cards:" + " " * 25 + "Dealer Cards:"
@@ -125,8 +55,87 @@ def display_cards(player_cards, dealer_cards)
   lines.each { |line| puts line }
 end
 
+def update_card_values(card)
+  c2, c4, c6, c7, c8, c9, c10, n10 = nil
+  num = card[1]
+  suit = determine_suit_code(card[0])
+  c2, c4, c6, c7, c8, c9, c10, n10 = update_numcard_values(num, suit)
+  num, c9 = update_facecard_values(num, suit, c9)
+  c2, c4, c6, c7, c8, c9, c10, n10 = [c2, c4, c6, c7, c8, c9, c10, n10].map { |x| x ||= " " }
+  [suit, num, c2, c4, c6, c7, c8, c9, c10, n10]
+end
+
+def determine_suit_code(suit)
+  case suit
+  when :spades then suit_code = "\u2660"
+  when :clubs then suit_code = "\u2663"
+  when :hearts then suit_code = "\u2665"
+  when :diamonds then suit_code = "\u2666"
+  end
+  suit_code
+end
+
+def update_numcard_values(num, suit)
+  case num
+  when "2" then c2 = c7 = suit
+  when "3" then c9 = c10 = suit
+  when "4" then c4 = suit
+  when "5" then c4 = c9 = suit
+  when "6" then c6 = suit
+  when "7" then c6 = c7 = suit
+  when "8" then c4 = c8 = suit
+  when "9" then c4 = c8 = c9 = suit
+  when "10"
+    c4 = c8 = c10 = suit
+    n10 = ""
+  end
+  [c2, c4, c6, c7, c8, c9, c10, n10]
+end
+
+def update_facecard_values(num, suit, c9)
+  case num
+  when "ace"
+    num = "A"
+    c9 = suit
+  when "king"
+    num = "K"
+    c9 = suit
+  when "queen"
+    num = "Q"
+    c9 = suit
+  when "jack"
+    num = "J"
+    c9 = suit
+  end
+  [num, c9]
+end
+
+def compile_lines(lines, cv)
+  lines[0] << CARD_FORMAT[0]
+  lines[1] << format(CARD_FORMAT[1], num: cv[1], c8: cv[6], n10: cv[9])
+  lines[2] << format(CARD_FORMAT[2], suit: cv[0], c6: cv[4], c10: cv[8])
+  lines[3] << format(CARD_FORMAT[3], c4: cv[3], c7: cv[5])
+  lines[4] << format(CARD_FORMAT[4], c6: cv[4], c9: cv[7])
+  lines[5] << format(CARD_FORMAT[5], c2: cv[2], c4: cv[3])
+  lines[6] << format(CARD_FORMAT[6], suit: cv[0], c6: cv[4], c10: cv[8])
+  lines[7] << format(CARD_FORMAT[7], num: cv[1], c8: cv[6], n10: cv[9])
+  lines[8] << CARD_FORMAT[8]
+end
+
+def compile_lines_hidden(lines, cv)
+  lines[0] << CARD_FORMAT[0].slice(0..2)
+  lines[1] << format(CARD_FORMAT[1], num: cv[1], c8: cv[6], n10: cv[9]).slice(0..2)
+  lines[2] << format(CARD_FORMAT[2], suit: cv[0], c6: cv[4], c10: cv[8]).slice(0..2)
+  lines[3] << format(CARD_FORMAT[3], c4: cv[3], c7: cv[5]).slice(0..2)
+  lines[4] << format(CARD_FORMAT[4], c6: cv[4], c9: cv[7]).slice(0..2)
+  lines[5] << format(CARD_FORMAT[5], c2: cv[2], c4: cv[3]).slice(0..2)
+  lines[6] << format(CARD_FORMAT[6], suit: cv[0], c6: cv[4], c10: cv[8]).slice(0..2)
+  lines[7] << format(CARD_FORMAT[7], num: cv[1], c8: cv[6], n10: cv[9]).slice(0..2)
+  lines[8] << CARD_FORMAT[8].slice(0..2)
+end
+
 def initial_prompt(balance)
-  system 'clear'
+  system 'clear' or system 'cls'
   puts "Balance: $#{balance}" + " " * 9 + "--TEALEAF BLACKJACK--"
   print "\nHow much would you like to wager: "
   bet = gets.chomp.to_i
@@ -143,19 +152,19 @@ def initial_prompt(balance)
   [bet, balance -= bet]
 end
 
-def determine_plays(player_cards, player_cards2, balance, bet)
+def determine_plays(player_cards, player_total2, balance, bet)
   valid_plays = %w(Hit Stand)
-  if player_cards2.empty?
-    valid_plays << "Split" if player_cards.length == 2 \
-                              && player_cards[0][2] == player_cards[1][2] && balance > bet
+  if player_total2 == 0
+    valid_plays << "Split" if player_cards.length == 2 &&
+                              (player_cards[0][2] == player_cards[1][2] ||
+                              player_cards[0][1] == player_cards[1][1]) && balance > bet
     valid_plays << "Double" if player_cards.length == 2 && balance > bet
   end
   valid_plays
 end
 
 def deal_cards(deck)
-  player_cards = [deck[0], deck[2]]
-  dealer_cards = [deck[1], deck[3]]
+  player_cards, dealer_cards = [deck[0], deck[2]], [deck[1], deck[3]]
   deck.shift(4)
   player_cards, player_total = determine_hand_values(player_cards)
   dealer_cards, dealer_total = determine_hand_values(dealer_cards)
@@ -172,7 +181,7 @@ def determine_hand_values(cards)
 end
 
 def update_hand(deck, balance, player_cards, player_cards2, player_total, player_total2, bet, bet2, stand)
-  play = prompt_user(player_cards, player_cards2, balance, bet)
+  play = prompt_user(player_cards, player_total2, balance, bet)
   case play
   when "Hit" then deck, player_cards, player_total = hit(deck, player_cards, player_total)
   when "Double"
@@ -210,6 +219,12 @@ end
 
 def hit(deck, player_cards, player_total)
   player_cards << deck[0]
+  player_cards, player_total = calculate_total(player_cards, player_total)
+  deck.shift
+  [deck, player_cards, player_total]
+end
+
+def calculate_total(player_cards, player_total)
   player_total += player_cards.last[2]
   if player_total > 21
     if player_cards.any? { |card| card[2] == 11}
@@ -217,8 +232,7 @@ def hit(deck, player_cards, player_total)
       player_cards.select { |card| card[2] == 11 }.first[2] = 1
     end
   end
-  deck.shift
-  [deck, player_cards, player_total]
+  [player_cards, player_total]
 end
 
 def winning_message(player_total, player_total2, dealer_total, bet, bet2, balance, name)
@@ -304,7 +318,7 @@ player_cards = player_cards2 = dealer_cards = []
 stand = stand2 = continue = false
 
 # Welcome user
-system 'clear'
+system 'clear' or system 'cls'
 puts "Welcome to Tealeaf Blackjack"
 print "\nPlease enter your name: "
 name = gets.chomp.capitalize
@@ -379,8 +393,8 @@ loop do
     balance = winning_message(player_total, player_total2, dealer_total, bet, bet2, balance, name)
     sleep 1
     continue = (1..999).include?(balance) ? prompt_continue?(name) : false
+    break if continue == 'n' || !(1..999).include?(balance)
   end
-  break if continue == 'n' || !(1..999).include?(balance) && stand && (stand2 || player_total2 == 0)
 end
 
 final_message(balance, stand, stand2, player_total2)
